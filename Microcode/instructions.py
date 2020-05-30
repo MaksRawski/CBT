101b80d90dd768293a4dc4182c494ff680091a51
i4mz3r0 <maksymilian.rawski1@gmail.com>
2020-05-30 17:30:17 +0200

    added register pairs and HMAR is now given first

--- Microcode/instructions.py
+++ Microcode/instructions.py
@@ -1,12 +1,8 @@
 from variables import *
 
-#TODO: make sure that all unused opcodes eg. 01 000 110 end up being just SR|PCC
-# problem with this microcode is that jumping can't be done to addresse farer away than 256 bytes. Jump on {flag} can only change LMAR
-# fixed though now a lot of things take waaaaaay more cycles
-
 #invert all inputs despite MI, PCC, ALU inputs and obviously the one(s) given that should be activated
-def CW(x):
-    return int(x)^((2**31-1)^MI^PCC^AL0^AL1^AL2^AL3)
+def CW(x = 0):
+    return ((1<<31)-1)^MI^PCC^AL0^AL1^AL2^AL3^int(x)
 
 def addData(data,opcode,ut,flag):
     DATA[(flag<<12)+(ut<<8)+(opcode)]=CW(data)
@@ -17,7 +13,7 @@ def fetch(utime):
     return{
         0: LPO|LAI,
         1: HPO|HAI,
-        2: II|MO
+        2: II|MO|PCC
     }[utime]
 
 
@@ -39,37 +35,71 @@ def mov(opcode, utime, flags):
     elif src==0b110: #mov to lcd as a command
         if dst==0b111: #actual src is 111-immediate operand
             data=[
-                HPO|HAI|PCC,
+                HPO|HAI,
                 LPO|LAI,
-
-                MO|LCE|LCM
+                MO|LCE|LCM|PCC
             ]
         elif dst==0b110: #00 110 110 - straight up bs
             return HLT
         else:
             data=[
-                RO[dst]|LCM|LCE # use dst as a command for lcd
+                RO[dst]|LCE|LCM # use dst as a command for lcd
             ]
 
     elif src==0b111: #src is imm, data opcode
         data=[
-            HPO|HAI|PCC,
+            HPO|HAI,
             LPO|LAI,
-            MO|(RI[dst]) # output from memory and set control bit of input of dst
+            MO|(RI[dst])|PCC 
         ]
+    elif dst==0b111:
+        # src = 0 xx 
+        # xx==00 dc
+        # xx==01 cb
+        # xx==10 ba
+        # xx==11 da
+        if src&0b11==0b00:
+            data=[
+                HPO|HAI,
+                LPO|LAI,
+                MO|DI|PCC,
+                MO|CI|PCC,
+            ]
+
+        elif src&0b11==0b01:
+            data=[
+                HPO|HAI,
+                LPO|LAI,
+                MO|CI|PCC,
+                MO|BI|PCC,
+            ]
+        elif src&0b11==0b10:
+            data=[
+                HPO|HAI,
+                LPO|LAI,
+                MO|BI|PCC,
+                MO|AI|PCC,
+            ]
+        elif src&0b11==0b11:
+            data=[
+                HPO|HAI,
+                LPO|LAI,
+                MO|DI|PCC,
+                MO|AI|PCC,
+            ]
 
     CF=(flags==0b0001)
     HF=(flags==0b0010)
     OF=(flags==0b0100)
     ZF=(flags==0b1000)
 
-    #conditional jumps
+    # (conditional) jumps
     if dst==0b101:
         if (src==0 and CF) or (src==1 and HF) or (src==2 and OF) or (src==3 and ZF) or (src==0b111):
             data=[
                 LPO|LAI|PCC,
                 HPO|HAI,
-                MO|ALM|ALE, #dst LPC is in memory but we save it in alu
+                MO|ALE, #dst LPC is in memory but we save it in alu
 
                 LPO|LAI|PCC,
                 HPO|HAI,
@@ -80,11 +110,11 @@ def mov(opcode, utime, flags):
         else:
             data=[]
   
-    data.append(SR|PCC)
+    data.append(SR)
     try:
         return data[utime]
     except IndexError:
-        return SR|PCC
+        return SR
 
 def load(opcode, utime, flags):
 
@@ -92,19 +122,50 @@ def load(opcode, utime, flags):
     src=(opcode&0b00000111)>>0
 
     data=[
-        RO[src]|LAI,
-        RI[dst]|MO 
+
+        
     ]
 
-    if src==0b111:
-        #this is the same thing as 00 xxx 111 but instead of putting into PC its put into LMAR and HMAR
+    if src<=0b011:
+        # src = 0 xx 
+        # xx==00 dc
+        # xx==01 cb
+        # xx==10 ba
+        # xx==11 da
+        if src==0b00:
+            data=[
+                DO|HAI,
+                CO|LAI,
+                MO|RI[dst]
+            ]
+        elif src==0b01:
+            data=[
+                CO|HAI,
+                BO|LAI,
+                MO|RI[dst]
+            ]
+        elif src==0b10:
+            data=[
+                BO|HAI,
+                AO|LAI,
+                MO|RI[dst]
+            ]
+        elif src==0b11:
+            data=[
+                DO|HAI,
+                AO|LAI,
+                MO|RI[dst]
+            ]
+        
+
+    elif src==0b111:
         data=[
-            LPO|LAI|PCC,
+            LPO|LAI,
             HPO|HAI, #first byte provided is now in memory (LMAR)
 
-            MO|ALM|ALE, #save LMAR into alu
+            MO|ALE|PCC, #save HMAR into alu
 
-            LPO|LAI|PCC,
+            LPO|LAI,
             HPO|HAI, #second byte provided is now in memory (HMAR)
 
             MO|HAI,  #HMAR is now what it's supposed to be
@@ -117,102 +178,146 @@ def load(opcode, utime, flags):
         #pop into register
         data=[ 
             AL1|AL0|ALE, #set alu to minus 1/0xFF (see 74181 docuemntation for details)
-            ALO|HAI, #set HAI to 0xff
+            ALO|HAI, #set HMAR to 0xff
+
+            SPO|ALM|ALC|ALE, #increment SP,by default S=0000 M=L Cn=L
+            ALO|SPI,
+
             SPO|LAI, #set LMAR to value in SP
             
-            RI[dst]|MO,  #pop the value into dst register
+            MO|RI[dst]  #pop the value into dst register
             
-            SPO|ALC|ALE, #increment SP,by default S=0000 M=L Cn=L
-            ALO|SPI
         ]
 
         if dst==0b101:
             #ret
             data=[
                 AL1|AL0|ALE, #set alu to minus 1/0xFF (see 74181 docuemntation for details)
-                ALO|HAI, #set HAI to 0xff
-                SPO|LAI, #set LMAR to value in SP
+                ALO|HAI, #set HMAR to 0xff
+
+                SPO|ALM|ALC|ALE, #increment SP,by default S=0000 M=L Cn=L
+                SPI|ALO|LAI, # increment SP and LMAR
 
                 LPI|MO,  #pop the value into LPC
 
                 SPO|ALC|ALE, #increment SP,by default S=0000 M=L Cn=L
-                SPI|ALO,
-                SPO|LAI, #set LMAR to value in SP
+                ALO|SPI,
 
-                HPI|MO, #pop the other value off stack into HPC
-
-                SPO|ALC|ALE, #increment SP,by default S=0000 M=L Cn=L
-                ALO|SPI
+                HPI|MO #pop the other value off stack into HPC
             ]
 
-    data.append(SR|PCC)
+    data.append(SR)
     try:
         return data[utime]
     except IndexError:
-        return SR|PCC
+        return SR
 
 def sto(opcode, utime, flags):
 
     dst=(opcode&0b00111000)>>3
     src=(opcode&0b00000111)>>0
 
-    data=[
-            RO[dst]|LAI,
-            RO[src]|MI
-    ]
-    if dst==0b111:    
-        #this is very similar thing to 01 xxx 111
+    if dst<=0b011:
+        # src = 0 xx 
+        # xx==00 dc
+        # xx==01 cb
+        # xx==10 ba
+        # xx==11 da
+        if dst==0b00:
+            data=[
+                DO|HAI,
+                CO|LAI,
+                MI|RO[src]
+            ]
+        elif src==0b01:
+            data=[
+                CO|HAI,
+                BO|LAI,
+                MI|RO[src]
+            ]
+        elif src==0b10:
+            data=[
+                BO|HAI,
+                AO|LAI,
+                MI|RO[src]
+            ]
+        elif src==0b11:
+            data=[
+                DO|HAI,
+                AO|LAI,
+                MI|RO[src]
+            ]
+        
+
+    elif dst==0b111:    
         data=[
-            LPO|LAI|PCC,
+            LPO|LAI,
             HPO|HAI, #first byte provided is now in memory (LMAR)
 
-            MO|ALM|ALE, #save LMAR into alu
+            MO|ALE|PCC, #save HMAR into alu
 
-            LPO|LAI|PCC,
+            LPO|LAI,
             HPO|HAI, #second byte provided is now in memory (HMAR)
 
-            MO|HAI,  #HMAR is now what it's supposed to be
-            ALO|LAI, #LMAR is now what it's supposed to be 
+            MO|LAI,  #LMAR is now what it's supposed to be
+            ALO|HAI, #HMAR is now what it's supposed to be 
 
             MI|RO[src] #store src
         ]
 
     elif dst==0b100:
-        #push src
-        data=[
-            AL1|AL0|ALE, #set alu to minus 1/0xFF (see 74181 docuemntation for details)
-            ALO|HAI, #set HAI to 0xff
-            SPO|LAI, #set LMAR to value in SP
 
-            RO[src]|MI, #push the value onto stack 
+        if src == 0b111: # push value
+            data=[
+                HPO|HAI,
+                LPO|LAI,
+                MO|ALE, # save given byte in alu
 
-            SPO|AL0|AL1|AL2|AL3|ALE, #decrement SP, S=1111 M=L Cn=H
-            SPI|ALO
-        ]
+                HAI, #set HMAR to 0xff
+                SPO|LAI, #set LMAR to value in SP
+
+                ALO|MI, #push the value onto stack 
+
+                SPO|ALM|AL0|AL1|AL2|AL3|ALE, #decrement SP, S=1111 M=L Cn=H
+                SPI|ALO
+            ]
+
+        
+        else: #push src register
+            data=[
+                AL1|AL0|ALE, #set alu to minus 1/0xFF (see 74181 documentation for details)
+                ALO|HAI, #set HMAR to 0xff
+                SPO|LAI, #set LMAR to value in SP
+
+                RO[src]|MI, #push the register onto stack 
+
+                SPO|ALM|AL0|AL1|AL2|AL3|ALE, #decrement SP, S=1111 M=L Cn=H
+                SPI|ALO
+            ]
+
         if dst==0b101:
             #call
             data=[
-                AL1|AL0|ALE, #set alu to minus 1/0xFF (see 74181 docuemntation for details)
-                ALO|HAI, #set HAI to 0xff
+                HAI, #set HMAR to 0xff
                 SPO|LAI, #set LMAR to value in SP
+                # MAR -> stack
 
-                HPO|MI,  #push HPC onto stack
+                HPO|MI, # push HPC
 
-                SPO|AL0|AL1|AL2|AL3|ALE, #decrement SP, S=1111 M=L Cn=H
-                SPI|ALO,
-                SPO|LAI, #set LMAR to value in SP
+                SPO|ALM|AL0|AL1|AL2|AL3|ALE, #decrement SP, S=1111 M=L Cn=H
+                SPI|ALO|LAI, # decrement LMAR and SP
 
-                LPO|MI, #push LPC onto stack
+                LPO|MI, # push LPC
 
-                SPO|ALC|ALE, #increment SP,by default S=0000 M=L Cn=L
-                ALO|SPI
+                SPO|ALM|AL0|AL1|AL2|AL3|ALE, #decrement SP, S=1111 M=L Cn=H
+                SPI|ALO # decrement SP
             ]
 
-    data.append(SR|PCC)
+    data.append(SR)
     try:
         return data[utime]
     except IndexError:
-        return SR|PCC
+        return SR
 
 
 def alu(opcode, utime, flags):
@@ -247,8 +352,8 @@ def alu(opcode, utime, flags):
         ALO|RI[src] #output of ALU operation goes to src register
     ]
 
-    data.append(SR|PCC)
+    data.append(SR)
     try:
         return data[utime]
     except IndexError:
-        return SR|PCC
+        return SR

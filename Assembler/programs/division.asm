97899ef7ee414e3085f35d402ae166158cc5f21c
i4mz3r0 <>
2020-06-20 21:16:11 +0200

    working program for division

--- Assembler/programs/division.asm
+++ Assembler/programs/division.asm
@@ -1,9 +1,9 @@
 ; function div8 - divide two 8-bit integers
-;  Rb: dividend
-;  Ra: divisor
+;  b: dividend
+;  a: divisor
 ; returns:
-;  Rc: result
-;  Rb: remainder
+;  c: result
+;  b: remainder
 
 #include "cbt.cpu"
 text: #str "8/2=\0"
@@ -24,6 +24,8 @@ main:
 	add c
 	mov lcd,c
 
+	hlt
+
 printStr:
 	load d,[cb]
 	inc b ; move pointer to next character
@@ -40,14 +42,18 @@ printStr:
 div8:
 	mov c, 0x00
 	
+	.step:
+		cmp b,a
+		;jz .return
+		jc .add ; if there is carry it means there is no borrow therefore we continue subtracting
+		jmp .return
+		
 	.add:
 		inc c
+		; b = b - a
+		sub b
+		jz .return
+		jmp .step
 
-		; b=b-a
-		sub b,a
-		jz .ret
-		jc .add ; if there is carry it means there is no borrow therefore we continue subtracting
-		jmp .ret ; if there was borrow then b is smaller than 0
-
-	.ret:
+	.return:
 		ret
